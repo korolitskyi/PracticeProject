1a9a85b08cc2d979bbb735957b02846feeea423e
Maksym Korolitskyi <maksym.korolitskyi@skysoft.tech>
2020-04-13 14:33:48 +0300

    Modified string enum

--- Test/StringEnum.cs
+++ Test/StringEnum.cs
@@ -6,11 +6,14 @@ namespace Test
 {
     public class StringEnum
     {
+        private static List<StringEnum> _values = new List<StringEnum>();
+
         private readonly string _value;
 
         private StringEnum(string value)
         {
             _value = value;
+            _values.Add(this);
         }
 
         public static readonly StringEnum Test1 = new StringEnum("test1");
@@ -23,6 +26,23 @@ namespace Test
         public static implicit operator string(StringEnum value)
         {
             return value._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is StringEnum && _value == ((StringEnum)obj)._value;
+        }
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+        public static bool operator ==(StringEnum left, StringEnum right)
+        {
+            return left._value == right._value;
+        }
+        public static bool operator !=(StringEnum left, StringEnum right)
+        {
+            return left._value != right._value;
         }
     }
 }
